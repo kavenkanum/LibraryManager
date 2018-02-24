0d3fe4dbfad0bc58b69e8495913762dc2dcf1372
ANETKAPC\Aneta <aneta.dabrowska@puredev.eu>
2018-02-24 15:29:49 +0100

    moving files from console application to web app

---
+++ WebApp/LibraryMVC.Domain/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryDomain
+{
+	public class Book
+	{
+		public int BookID { get; set; }
+		public string Name { get; set; }
+		public string Author { get; set; }
+		public int NumberAvailableBooks { get; set; }
+	}
+}
