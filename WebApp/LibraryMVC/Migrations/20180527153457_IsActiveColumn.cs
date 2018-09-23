88b7a8d8f1a3180a44e25c2345b522ae34c316a8
ANETKAPC\Aneta <aneta.dabrowska@puredev.eu>
2018-09-23 20:25:22 +0200

    MIGRATION vol.2

---
+++ WebApp/LibraryMVC/Migrations/20180527153457_IsActiveColumn.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryMVC.Migrations
+{
+    public partial class IsActiveColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Books_BorrowedBooksID",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_BorrowedBooksID",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "BorrowedBooksID",
+                table: "Users");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BorrowedBooksID",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_BorrowedBooksID",
+                table: "Users",
+                column: "BorrowedBooksID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Books_BorrowedBooksID",
+                table: "Users",
+                column: "BorrowedBooksID",
+                principalTable: "Books",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
