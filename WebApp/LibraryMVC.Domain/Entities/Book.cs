2a42e53dd9fc703be59e4ba1b4a12a08b64a75ee
ANETKAPC\Aneta <aneta.dabrowska@puredev.eu>
2018-09-23 20:26:40 +0200

    BOOK

---
+++ WebApp/LibraryMVC.Domain/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryMVC.Domain.Models
+{
+	public class Book
+	{
+		public int ID { get; set; }
+
+        //[Required(ErrorMessage = "Please enter the user's first name.")]
+        //[StringLength(5, ErrorMessage = "The First Name must be less than {1} characters.")]
+        //[Display(Name = "First Name:")]
+
+        public string Name { get; set; }
+		public string Author { get; set; }
+		public int NumberAvailableBooks { get; set; }
+        public IEnumerable<BorrowedBook> BorrowingUsers { get; set; }
+
+
+	}
+}
