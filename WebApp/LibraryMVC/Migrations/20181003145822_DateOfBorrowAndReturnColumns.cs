b9e936c5fd327098d6237186092b622b57480a6c
ANETKAPC\Aneta <aneta.dabrowska@puredev.eu>
2018-10-05 15:29:27 +0200

    migration

---
+++ WebApp/LibraryMVC/Migrations/20181003145822_DateOfBorrowAndReturnColumns.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryMVC.Migrations
+{
+    public partial class DateOfBorrowAndReturnColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBorrow",
+                table: "BorrowedBooks",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: DateTime.UtcNow.AddDays(-1));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfReturn",
+                table: "BorrowedBooks",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBorrow",
+                table: "BorrowedBooks");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfReturn",
+                table: "BorrowedBooks");
+        }
+    }
+}
