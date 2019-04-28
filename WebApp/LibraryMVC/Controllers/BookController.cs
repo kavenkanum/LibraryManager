e7bc7e8d837a626322081fb54588b03a53125202
Unknown <aneta.dabrowska@puredev.eu>
2019-04-28 20:21:06 +0200

    New feature
    Add Image

--- WebApp/LibraryMVC/Controllers/BookController.cs
+++ WebApp/LibraryMVC/Controllers/BookController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using LibraryMVC.Domain.Models;
 using LibraryMVC.Domain.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryMVC.Controllers
@@ -15,7 +17,7 @@ namespace LibraryMVC.Controllers
         {
             bookRepository = _bookRepository;
         }
-       
+
         public IActionResult View(int id)
         {
             var book = bookRepository.Find(id);
@@ -47,37 +49,68 @@ namespace LibraryMVC.Controllers
 
         [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
-
         public IActionResult Edit(Book book)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 bookRepository.Edit(book);
                 bookRepository.Commit();
                 return RedirectToAction("List", "Books");
             }
             return View(book);
-            
         }
 
-        public IActionResult AddDescription(Book book)
+        public IActionResult AddDescription(int id)
         {
+            Book book = bookRepository.Find(id);
             return View(book);
         }
 
         [HttpPost]
         public IActionResult AddDescription(AddDescriptionModel model)
         {
+
             bookRepository.AddDescritpion(model.Id, model.Description);
-            return RedirectToAction("View", new { model.Id});
+            return RedirectToAction("View", new { model.Id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddImage(AddImageModel model)
+        {
+            var book = bookRepository.Find(model.Id);
+            using (var memoryStream = new MemoryStream())
+            {
+                await model.Image.CopyToAsync(memoryStream);
+                byte[] imageArray = memoryStream.ToArray();
+                book.Image = Convert.ToBase64String(imageArray);
+            }
+
+            bookRepository.Commit();
+            return RedirectToAction("View", new { model.Id });
+        }
+
+        [HttpGet]
+        public IActionResult AddImage(int id)
+        {
+            return View(new AddImageModel()
+            {
+                Id = id
+            });
         }
 
     }
 
+    
     public class AddDescriptionModel
     {
         public int Id { get; set; }
         public string Description { get; set; }
     }
-    
+
+    public class AddImageModel
+    {
+        public int Id { get; set; }
+        public IFormFile Image { get; set; }
+    }
+
 }
---
+++ WebApp/LibraryMVC/HtmlHelperExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace LibraryMVC
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string ToBase64Image(this IHtmlHelper htmlHelper, string image)
+        {
+            return $"data:image/png;base64, {image}";
+        }
+    }
+}
