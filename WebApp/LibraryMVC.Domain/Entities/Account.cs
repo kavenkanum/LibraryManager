737f74157530d304b7b665aa92cb45b00e9d9146
Unknown <aneta.dabrowska@puredev.eu>
2019-05-19 19:59:18 +0200

    Feature: User Account

---
+++ WebApp/LibraryMVC.Domain/Entities/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LibraryMVC.Domain.Models
+{
+    public class Account
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź nickname dla swojego konta")]
+        [StringLength(30, MinimumLength = 3)]
+        public string NickName { get; set; }
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ WebApp/LibraryMVC/Migrations/20190519175246_UserRegistrationAndLogin.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryMVC.Migrations
+{
+    public partial class UserRegistrationAndLogin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NickName",
+                table: "Users");
+
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NickName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NickName",
+                table: "Users",
+                nullable: true);
+        }
+    }
+}
