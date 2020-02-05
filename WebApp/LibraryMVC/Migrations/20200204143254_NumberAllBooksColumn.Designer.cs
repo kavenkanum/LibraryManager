2104e6be791ab468f08186245f30d86f3ae2fe01
Aneta <aneta.dabrowska@puredev.eu>
2020-02-05 10:09:29 +0100

    Feature: Migration to DB - NumberAllBooksColumn

--- WebApp/LibraryMVC/Migrations/20200204143254_NumberAllBooksColumn.Designer.cs
+++ WebApp/LibraryMVC/Migrations/20200204143254_NumberAllBooksColumn.Designer.cs
@@ -4,14 +4,16 @@ using LibraryMVC.Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LibraryMVC.Migrations
 {
     [DbContext(typeof(LibraryDbContext))]
-    partial class LibraryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200204143254_NumberAllBooksColumn")]
+    partial class NumberAllBooksColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
